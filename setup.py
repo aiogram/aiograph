9b2ea0cff469f409f86465b59db4463ec4372b7c
Alex Root Junior <jroot.junior@gmail.com>
2018-05-11 01:29:15 +0300

    Add upload command to setup.py (via twine)

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import functools
+import os
 import pathlib
 import re
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
 from setuptools.command.test import test as TestCommand
 
 try:
@@ -20,6 +21,7 @@ if sys.version_info < MINIMAL_PY_VERSION:
     raise RuntimeError('aiograph works only with Python {}+'.format('.'.join(map(str, MINIMAL_PY_VERSION))))
 
 
+@functools.lru_cache()
 def get_version():
     """
     Read version
@@ -70,6 +72,55 @@ class PyTest(TestCommand):
         raise SystemExit(errno)
 
 
+class UploadCommand(Command):
+    command_name = 'upload'
+    description = 'upload the distribution with the Python package index'
+
+    user_options = [
+        ('set-tag', 't', 'set Git tag')
+    ]
+
+    def initialize_options(self):
+        self.path = WORK_DIR / 'dist'
+        self.lib_version = get_version()
+        self.set_tag = False
+
+    def finalize_options(self):
+        opts = self.distribution.get_option_dict(self.get_command_name())
+
+        self.set_tag = 'set_tag' in opts
+
+    def find_target(self):
+        if not self.path.is_dir():
+            return
+
+        targets = []
+        for file in os.listdir(self.path):
+            if self.lib_version in file:
+                targets.append(str((self.path / file).absolute()))
+        return targets
+
+    def upload(self, targets):
+        import subprocess
+        errno = subprocess.call(['twine', 'upload'] + targets)
+        return errno
+
+    def create_tag(self):
+        import subprocess
+        errno = subprocess.call(['git', 'tag', f"v{self.lib_version}"])
+        return errno
+
+    def run(self):
+        targets = self.find_target()
+        if targets:
+            errno = self.upload(targets)
+            if not errno and self.set_tag:
+                errno = self.create_tag()
+            raise SystemExit(errno)
+        else:
+            raise FileNotFoundError('No files to be uploaded!')
+
+
 setup(
     name='aiograph',
     version=get_version(),
@@ -101,6 +152,7 @@ setup(
         'dev': get_requirements('dev_requirements.txt')
     },
     cmdclass={
-        'test': PyTest
+        'test': PyTest,
+        UploadCommand.command_name: UploadCommand
     }
 )
